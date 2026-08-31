efaba184ef3779ac1d8856f40b804aa64c32aae4
Akshansh Kakkar <akshanshkakkar92@gmail.com>
2026-08-31 20:00:34 +0530

    initial commit

---
+++ apps/server/src/index.ts
@@ -0,0 +1,39 @@
+import { createContext } from "@home2ocean/api/context";
+import { appRouter } from "@home2ocean/api/routers/index";
+import { auth } from "@home2ocean/auth";
+import { env } from "@home2ocean/env/server";
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
+import { toNodeHandler } from "better-auth/node";
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: env.CORS_ORIGIN,
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
+
+app.all("/api/auth{/*path}", toNodeHandler(auth));
+
+app.use(
+  "/trpc",
+  createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  }),
+);
+
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.status(200).send("OK");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
